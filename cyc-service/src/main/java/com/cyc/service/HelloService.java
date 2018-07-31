17704028714f11d856ed59b064f08006d27e7086
Khandaker Sultana <pavser@yahoo.com>
2018-07-31 00:37:26 -0500

    Add javadoc

--- cyc-service/src/main/java/com/cyc/service/HelloService.java
+++ cyc-service/src/main/java/com/cyc/service/HelloService.java
@@ -2,6 +2,10 @@ package com.cyc.service;
 
 public class HelloService
 {
+    /**
+     * This method returns a greeting
+     * @return A string message
+     */
     public String getGreeting()
     {
         return "Hello CYC";
